d32622368dbb91d7b5895cd4c76161c14ae768aa
jdsantana <jdsantana@estudiantes.uci.cu>
2014-01-14 02:16:31 -0500

    He activado el datafixtures bundle y el jsrouting. He creado un ejemplo de controlador y su formulario. He agregado datos de prueba.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
             new DBOJ\BackendBundle\BackendBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/DBOJ/BackendBundle/Controller/RoleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace GECOMET\SeguridadBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Rol controller.
+ *
+ */
+class RolController extends Controller
+{
+    /**
+     * Lists all Rol entities.
+     *
+     */
+    public function allAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $entities = $em->getRepository('SeguridadBundle:Rol')->findAll();
+        
+        $data = array();
+        foreach($entities as $entity){
+            $data[] = array(
+                'id' => $entity->getId(),
+                'rol' => $entity->getRol(),
+                'descripcion' => $entity->getDescripcion()
+            );
+        }
+        
+        $response = new Response(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
